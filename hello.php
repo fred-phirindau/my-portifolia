f85bcaef97b263cda08642874938bd8287744127
fred-phirindau <133360021+fred-phirindau@users.noreply.github.com>
2023-06-17 11:15:20 +0200

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        body{
+            background-color:rgba(255, 255, 255, 0.514);
+        }
+    </style>
+    <body>
+     <center>  <h1> some of the  things that i have done are the follows</h1>
+        <table border="strong"height="100" width="200">
+            <tr>
+                <th> name services being offered</th>
+                <th> cost</th>
+                <th> details</th>
+
+            </tr>
+            <tr>
+                <td>web application</td>
+                <td>k25000</td>
+                <td> contact mefred @gmail.com</td>
+            </tr>
+            <tr> 
+                <td>java application</td>
+                <td>k40000</td>
+                <td>099984589</td>
+            </tr>
+
+        </table>
+        <img src="istockphoto-1389603578-1024x1024.jpg" height="300px",width="300px".>
+        <hr>
+        <div> 
+            <h2> about me </h2>
+           <pre> if you want to know more about me  visit my web page and contact me every tHing will be settled 
+            DO
+           </pre>
+
+        </div></center>
+    </body>
+
+</html>
